48cc3be85a47b9590f8fd687280e2098de11394c
Anna Myzika <myzikaanya@gmail.com>
2020-10-10 21:24:41 +0300

    добавлено решение 5-й задачи 3-го модуля

--- js/05-task.js
+++ js/05-task.js
@@ -1,15 +1,32 @@
-function checkForSpam(str) {
+function getAllPropValues(array, prop) {
   "use strict";
   // Write code under this line
-  return (
-    str.toLowerCase().includes("spam") || str.toLowerCase().includes("sale")
-  );
+  let result = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (prop in array[i]) {
+      result.push(array[i][prop]);
+    }
+  }
+  return result;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
+// Объекты и ожидаемый результат
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Радар", price: 1280, quantity: 2 },
+  { name: "Радар", price: 1320, quantity: 1 },
+  { name: "Сканер", price: 2700, quantity: 1 },
+  { name: "Сканер", price: 2500, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
 
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(getAllPropValues(products, "name"));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
 
-console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(getAllPropValues(products, "quantity"));
+// [4, 2, 1, 1, 3, 7, 2]
 
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(getAllPropValues(products, "category"));
+//  []
