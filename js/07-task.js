124199a3a76917fada2c5d227ea4c5afbe26285a
Anna Myzika <myzikaanya@gmail.com>
2020-10-18 00:16:08 +0300

    добавлено решение 1-й задачи 4-го модуля

--- js/07-task.js
+++ js/07-task.js
@@ -1,28 +1,61 @@
-function filterArray(array) {
-  "use strict";
-  const numbers = [];
-  for (let i = 0; i < array.length; i += 1) {
-    // Write code under this line
-    if (Number.isFinite(array[i]) == true) {
-      let x = 0;
-      x = array[i];
-      numbers.push(x);
-    }
-  }
-  return numbers;
-}
+/*
+ * Типов транзацкий всего два.
+ * Можно положить либо снять деньги со счета.
+ */
+const Transaction = {
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
+};
 
-console.log(filterArray([-2, 0, 2]));
-// [-2, 0, 2]
+/*
+ * Каждая транзакция это объект со свойствами: id, type и amount
+ */
 
-console.log(filterArray([1, NaN, Infinity]));
-// [1]
+const account = {
+  // Текущий баланс счета
+  balance: 0,
 
-console.log(filterArray([0, -0, 100, "100"]));
-// [0, 0, 100]
+  // История транзакций
+  transactions: [],
 
-console.log(filterArray([undefined, false, null, [], 1]));
-// [1]
+  /*
+   * Метод создает и возвращает объект транзакции.
+   * Принимает сумму и тип транзакции.
+   */
+  createTransaction(amount, type) {},
 
-console.log(filterArray([{}, () => {}, 2]));
-// [2]
+  /*
+   * Метод отвечающий за добавление суммы к балансу.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций
+   */
+  deposit(amount) {},
+
+  /*
+   * Метод отвечающий за снятие суммы с баланса.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций.
+   *
+   * Если amount больше чем текущий баланс, выводи сообщение
+   * о том, что снятие такой суммы не возможно, недостаточно средств.
+   */
+  withdraw(amount) {},
+
+  /*
+   * Метод возвращает текущий баланс
+   */
+  getBalance() {},
+
+  /*
+   * Метод ищет и возвращает объект транзации по id
+   */
+  getTransactionDetails(id) {},
+
+  /*
+   * Метод возвращает количество средств
+   * определенного типа транзакции из всей истории транзакций
+   */
+  getTransactionTotal(type) {},
+};
