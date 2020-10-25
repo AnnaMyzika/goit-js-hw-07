24438a75fbe19a5607b6a55d7874f097cbd594f3
Anna Myzika <myzikaanya@gmail.com>
2020-10-25 23:41:28 +0300

    добавлено решение 3-й задачи 5-го модуля

--- js/03-task.js
+++ js/03-task.js
@@ -1,34 +1,58 @@
-const add = (accum, element) => accum + element;
-const mult = (accum, element) => accum * element;
-const sub = (accum, element) => accum - element;
-
-function reduceArray(array, cb, initial) {
-  'use strict';
-  let i;
-  let accum;
-  if(arguments.length >= 3) {
-    accum = initial;
-    i = 0;
+ // Write code under this line
+class Storage {
+  constructor(items) {
+    this.items = items;
   }
-  if(arguments.length === 2) {
-    accum = array[0];
-    i = 1;
+  
+  getItems() {
+    return this.items;
   }
-  for(i; i < array.length; i += 1) {
-    const element = array[i];
-    // Write code under this line
-    accum = (cb(accum, element));
+
+  addItem(item) {
+    return this.items.push(item);
   }
-  return accum;
-}
 
-const arr  = [1,2,3,4,5];
+  removeItem(item) {
+    return this.items.splice(1, 1);
+  }
+  }
 
-console.log(reduceArray(arr, add)); // 15
-console.log(reduceArray(arr, add, 10)); // 25
+console.log(typeof Storage);
+// 'function'
 
-console.log(reduceArray(arr, mult)); // 120
-console.log(reduceArray(arr, mult, 10)); // 1200
+const goods = [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+];
 
-console.log(reduceArray(arr, sub)); // -13
-console.log(reduceArray(arr, sub, 10)); // -5
+const storage = new Storage(goods);
+
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem('Пролонгер');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
